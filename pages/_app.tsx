8fed02decbaf39704f4c7255b000601143fbe203
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-07-27 20:34:54 +0400

    style: configure starting project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { locale } = useRouter();
+  return (
+    <div
+      className={locale === 'en' ? 'font-helvetica_en' : 'font-helvetica_ge'}
+    >
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
