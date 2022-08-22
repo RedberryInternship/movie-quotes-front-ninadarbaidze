e34eeebcf2d4721479178e71fc5fef3a51257a64
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-08-22 20:01:45 +0400

    feat: implement socket io

--- components/Movies/ListOfMovies/types.d.ts
+++ components/Movies/ListOfMovies/types.d.ts
@@ -1,6 +1,14 @@
 export type MovieTypes = {
-  [x: string]: { movieName: string };
+  [x?: string]: { movieName?: string };
   _id: string;
   year?: number;
+  image: string;
+};
+
+export type UpdatedMovieTypes = {
+  [x?: string]: { movieName?: string };
+  movieName?: string;
+  id: string;
+  year: number;
   image: string;
 };
--- components/Movies/ListOfMovies/useListOfMovies.ts
+++ components/Movies/ListOfMovies/useListOfMovies.ts
@@ -4,13 +4,15 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { getMovies } from 'services';
-import { MovieTypes } from './types';
+import { UpdatedMovieTypes } from './types';
+
+import openSocket from 'socket.io-client';
 
 export const useListOfMovies = () => {
   const movieCtx = useContext(MovieContext);
   const { t } = useTranslation();
   const ctx = useContext(AuthContext);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<UpdatedMovieTypes[]>([]);
   const [movieSum, setMovieSum] = useState<number>(0);
   const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
@@ -20,14 +22,14 @@ export const useListOfMovies = () => {
     setSearchQuery(event.target.value);
   };
 
+  let currentLan = router.locale;
   useEffect(() => {
     const getData = async () => {
-      let currentLan = router.locale;
       let token = session ? session.accessToken : ctx.token;
       try {
         const response = await getMovies(token as string);
         const movieNumber = response.data.length;
-        const newData = response.data.map((movies: MovieTypes) => {
+        const newData = response.data.map((movies: UpdatedMovieTypes) => {
           return {
             id: movies._id,
             movieName: movies[currentLan!].movieName,
@@ -40,15 +42,31 @@ export const useListOfMovies = () => {
       } catch (err: any) {}
     };
     getData();
-  }, [
-    ctx.token,
-    ctx.userId,
-    router.locale,
-    movieCtx.movieAdded,
-    setMovieSum,
-    setData,
-    session,
-  ]);
+  }, [ctx.token, ctx.userId, currentLan, session]);
+
+  useEffect(() => {
+    const socket = openSocket(`${process.env.NEXT_PUBLIC_API_URL}`);
+    socket.on('movies', (data) => {
+      const movie = {
+        id: data.movie,
+        movieName: data.movie[currentLan!].movieName,
+        year: data.movie.year,
+        image: data.movie.image,
+      };
+      if (data.action === 'create') {
+        addMovie(movie);
+      }
+    });
+  }, [currentLan]);
+
+  const addMovie = (movie: UpdatedMovieTypes) => {
+    setData((prevState) => {
+      let updatedMovies: UpdatedMovieTypes[] = [];
+      updatedMovies = [...prevState];
+      updatedMovies!.unshift(movie);
+      return updatedMovies;
+    });
+  };
 
   const openMovieForm = () => {
     movieCtx.movieCreationStateHandler();
