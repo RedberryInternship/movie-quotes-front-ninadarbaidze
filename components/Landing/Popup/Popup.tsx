2fa656b054a674c4998e11a5e0f390b246d8c1a0
Nyx <33086430+ninadarbaidze@users.noreply.github.com>
2022-09-09 16:03:06 +0400

    style: fix modal design (#38)

--- components/Landing/Popup/Popup.tsx
+++ components/Landing/Popup/Popup.tsx
@@ -10,16 +10,16 @@ const Popup: React.FC<PopupTypes> = (props) => {
   return (
     <>
       <Backdrop />
-      <Modal className='flex flex-col items-center justify-center fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-profileDialog rounded-[10px]  z-40 w-[79%] md:w-[55%] lg:w-[45%] xl:w-[35%]'>
-        <div className='flex flex-col justify-center items-center w-full gap-8 '>
+      <Modal className='flex flex-col items-center justify-center fixed top-[30%] md:top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-profileDialog rounded-[10px] w-[20rem] z-40 md:w-[28rem] md:h-[22rem]'>
+        <div className='flex flex-col justify-center items-center w-full  gap-8 '>
           <div className='mt-6'>
             <Image src={icon} alt='popup-icon' width={70} height={70} />
           </div>
-          <div className='flex flex-col items-center gap-4 px-4 w-full lg:w-[85%]'>
+          <div className='flex flex-col items-center gap-4 w-full px-10 md:px-14'>
             <h1 className='text-2xl md:text-3xl font-bold text-white text-center'>
               {heading}
             </h1>
-            <h3 className='text-base md:text-lg text-center w-[90%] text-white'>
+            <h3 className='text-base md:text-lg text-center text-white'>
               {paragraph}
             </h3>
             <div className='mb-6'>
@@ -27,7 +27,7 @@ const Popup: React.FC<PopupTypes> = (props) => {
                 <a>
                   <Button
                     text={buttonTxt}
-                    className='w-56 py-2  bg-red hover:bg-redHover'
+                    className='w-[15rem] md:w-[21rem] py-2  bg-red hover:bg-redHover'
                   />
                 </a>
               </Link>
